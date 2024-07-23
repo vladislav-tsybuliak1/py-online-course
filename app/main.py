2a38ff89def023598612a043dbc92b758140bfd3
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-07-23 08:40:03 +0300

    implement OnlineCourse class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+from __future__ import annotations
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> OnlineCourse:
+        return cls(
+            name=course_dict["name"],
+            description=course_dict["description"],
+            weeks=cls.days_to_weeks(course_dict["days"])
+        )
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return (days - 1) // 7 + 1
